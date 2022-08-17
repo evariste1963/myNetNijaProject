f57532bdb88ec5c30be8e197a566a98f4b5c981c
evariste1963 <evariste@hotmail.co.uk>
2022-08-17 20:58:42 +0100

    initial commit and upto lesson 4 inc

---
+++ src/App.svelte
@@ -0,0 +1,43 @@
+<script>
+	let name = 'this.me';
+	let beltColour = 'black'
+	const handleClick = () => {
+		beltColour = 'orange'
+	}
+	const handleInput = (e) => {
+		beltColour = e.target.value
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<!-- dynamic inline style -->
+	<p style='color:{beltColour}'>{beltColour} belt</p>
+	<button on:click={handleClick}>update belt colour</button>
+	<!-- 2 way binding --- 2 ways -->
+	 <!-- <input on:input={handleInput} type="text" value = {beltColour}> -->
+	 <!-- short method of 2 way binding-->
+	 <input type= "text" bind:value={beltColour}>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
